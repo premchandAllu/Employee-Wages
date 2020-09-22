59807f84ca39795b4b179e57c40e8af7a2cb6f6d
premchandAllu <premchandallu1999@gmail.com>
2020-09-22 23:19:40 +0530

    Employee wages using swicth case

--- Employee_wages.java
+++ Employee_wages.java
@@ -2,31 +2,32 @@ package project1;
 
 public class Employee_wages
   { public static void main(String[] args)
-	 { 
-	        //Constants
-			int IS_PART_TIME=1;
-			int IS_FULL_TIME=2;
-			int WAGE_PER_HOUR=20;
-			
-			//Variables
-			int emphours;
-			int empwage;
-			
-			//Computation
-			int empcheck= (int) ( Math.floor(Math.random() * 10) %3 );
-			 System.out.println("Employee Status is "+empcheck);
-			 
-			   if(empcheck==IS_PART_TIME)
-			     { emphours=8;}
-			   else if(empcheck==IS_FULL_TIME)
-			     { emphours=8;}
-			   else
-			     { emphours=0;}
-			   
-			 empwage=emphours*WAGE_PER_HOUR;
-			 
-			 //Printing Output
-	        System.out.println("Employee wage is "+empwage);
-	 }
-    
+	 {   //Constant
+		 int WAGE_PER_HOUR=20;
+		 
+		 //Variables
+		 int emphours;
+		 int empwage;
+		 
+		 //Computation
+		 int empcheck= (int) (Math.floor( Math.random() * 10) % 3);
+		 switch (empcheck)
+		   {
+		    case 1 :
+		       { emphours=8; 
+		          break ; }
+		    case 2 :
+		       { emphours=8; 
+		          break ; }
+		    default :
+		       { emphours=0;
+		          break ;}
+		   }
+		 empwage= emphours*WAGE_PER_HOUR;
+		 
+		 //Printing output
+		 System.out.println("Employee status is "+empcheck);
+		 System.out.println("Employee Wage is "+empwage);
+
+	   }
 }
