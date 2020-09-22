d6bbfb5396ef3c35c86b2e82ed6565470652caa2
premchandAllu <premchandallu1999@gmail.com>
2020-09-22 22:36:26 +0530

    Employee is Present or Absent

--- Employee_wages.java
+++ Employee_wages.java
@@ -3,7 +3,17 @@ package project1;
 public class Employee_wages
   { public static void main(String[] args)
 	  { 
-	    System.out.println("Welcome to Employee Wage Computation program");
+	  // Constant
+		int IS_FULL_TIME=1;
+		 // Computation
+		double empcheck =Math.floor(Math.random() *10)% 2;
+		 
+		if(empcheck==IS_FULL_TIME)
+		     System.out.println("Employee is present");
+	       
+		else 
+			 System.out.println("Employee is absent");
+	
 	  }
     
 }
