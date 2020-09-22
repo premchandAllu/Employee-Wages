01efa8aeb6ffb48023cd9409f41ac38d001aa97b
premchandAllu <premchandallu1999@gmail.com>
2020-09-22 23:03:31 +0530

    Part time Employee Wages is added

--- Employee_wages.java
+++ Employee_wages.java
@@ -3,26 +3,30 @@ package project1;
 public class Employee_wages
   { public static void main(String[] args)
 	 { 
-	   //Constants
-	   int IS_FULL_TIME =1;
-	   int WAGE_PER_HOUR = 20;
-	   
-	   //Variables
-	   int empwage;
-	   int emphours;
-	   
-	   // Computation
-	   double empcheck=Math.floor(Math.random() *10)%2;
-	    
-       if(empcheck==IS_FULL_TIME)
-           {   emphours=8; }
-       else 
-           { 	 emphours=0; }
-        
-        empwage= emphours * WAGE_PER_HOUR;
-        
-      // Printing output
-       System.out.println("Daily Employee Wage is"+empwage); 
+	        //Constants
+			int IS_PART_TIME=1;
+			int IS_FULL_TIME=2;
+			int WAGE_PER_HOUR=20;
+			
+			//Variables
+			int emphours;
+			int empwage;
+			
+			//Computation
+			int empcheck= (int) ( Math.floor(Math.random() * 10) %3 );
+			 System.out.println("Employee Status is "+empcheck);
+			 
+			   if(empcheck==IS_PART_TIME)
+			     { emphours=8;}
+			   else if(empcheck==IS_FULL_TIME)
+			     { emphours=8;}
+			   else
+			     { emphours=0;}
+			   
+			 empwage=emphours*WAGE_PER_HOUR;
+			 
+			 //Printing Output
+	        System.out.println("Employee wage is "+empwage);
 	 }
     
 }
