b5a9ad471a05e590dd2c3e99a76057cca860b8bc
premchandAllu <premchandallu1999@gmail.com>
2020-09-22 22:25:36 +0530

    Welcome to Employee Wages Computation program

---
+++ Employee_wages.java
@@ -0,0 +1,9 @@
+package project1;
+
+public class Employee_wages
+  { public static void main(String[] args)
+	  { 
+	    System.out.println("Welcome to Employee Wage Computation program");
+	  }
+    
+}
