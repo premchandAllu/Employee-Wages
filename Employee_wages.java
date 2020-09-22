35f9f59e7f268a85c3c78c9a597b82fa2232f876
premchandAllu <premchandallu1999@gmail.com>
2020-09-22 22:47:47 +0530

    Employee Wage is calculated

--- Employee_wages.java
+++ Employee_wages.java
@@ -2,18 +2,27 @@ package project1;
 
 public class Employee_wages
   { public static void main(String[] args)
-	  { 
-	  // Constant
-		int IS_FULL_TIME=1;
-		 // Computation
-		double empcheck =Math.floor(Math.random() *10)% 2;
-		 
-		if(empcheck==IS_FULL_TIME)
-		     System.out.println("Employee is present");
-	       
-		else 
-			 System.out.println("Employee is absent");
-	
-	  }
+	 { 
+	   //Constants
+	   int IS_FULL_TIME =1;
+	   int WAGE_PER_HOUR = 20;
+	   
+	   //Variables
+	   int empwage;
+	   int emphours;
+	   
+	   // Computation
+	   double empcheck=Math.floor(Math.random() *10)%2;
+	    
+       if(empcheck==IS_FULL_TIME)
+           {   emphours=8; }
+       else 
+           { 	 emphours=0; }
+        
+        empwage= emphours * WAGE_PER_HOUR;
+        
+      // Printing output
+       System.out.println("Daily Employee Wage is"+empwage); 
+	 }
     
 }
