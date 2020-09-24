ab2a4f64ebec85a79cbd2dbcc7eeaa99e083aec0
premchandAllu <premchandallu1999@gmail.com>
2020-09-24 10:19:31 +0530

    wages calculated for a condition of 100 hours and 20 days

--- Employee_wages.java
+++ Employee_wages.java
@@ -1,32 +1,46 @@
 package project1;
 
 public class Employee_wages
-  { public static void main(String[] args)
-	 {   //Constants
-	     int WAGE_PER_HOUR=20;
-	     int NO_OF_DAYS=20;
-	     
+  { 	  //Constants
+	      public static final  int WAGE_PER_HOUR=20;
+	      public static final int NO_OF_DAYS=20;
+	      public static final int NO_OF_HOURS=100;
+	      public static final int IS_FULL_TIME=1;
+	      public static final int IS_PART_TIME=2;
+	      
+	   public static void main(String[] args)
+	    { 
 	     //Variables
 	     int emphours;
+	     int empwage;
 	     
 	     //Computation
 	     int empcheck = (int) (Math.floor(Math.random() * 10 ) % 3);
 	     
 	     switch (empcheck)
 	     { 
-	     case 1 :
-	     { emphours =8;
+	     case IS_FULL_TIME :
+	     { emphours =8; 
+	       System.out.println("Employee status -Full time");
 	        break; }
-	     case 2 :
-	     { emphours=8;
+	     case IS_PART_TIME :
+	     { emphours=4;
+	       System.out.println("Employee status -Part time");
 	       break ; }
 	     default :
 	     { emphours =0;
-	       break ; }
+	       System.out.println("Employee status -Absent");
+	       System.exit(0); }
 	     }
-	     int monthly_wage = WAGE_PER_HOUR * emphours * NO_OF_DAYS ;
-	     //Printing Output
-	     System.out.println("Employee status is"+empcheck);
-	     System.out.println("Monthly Wage of Employee is "+monthly_wage);
-	   }
+	    
+	     if(NO_OF_HOURS/emphours < NO_OF_DAYS && emphours!=0)
+	     { empwage= NO_OF_HOURS* WAGE_PER_HOUR;
+	       System.out.println("Employee wage for 100 hours is "+empwage);
+	       
+	     }
+	     else if(NO_OF_HOURS/emphours >= NO_OF_DAYS && emphours !=0)
+	     { empwage= emphours*WAGE_PER_HOUR*NO_OF_DAYS ;
+	       System.out.println("Employee wage for 20 days is"+empwage);
+	       }
+	     }
 }
