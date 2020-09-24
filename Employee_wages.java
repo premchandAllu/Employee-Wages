1367222e5e104fe14dd44aa9454ec334f70a7b06
premchandAllu <premchandallu1999@gmail.com>
2020-09-24 09:40:36 +0530

    Monthly wages are calculated

--- Employee_wages.java
+++ Employee_wages.java
@@ -2,32 +2,31 @@ package project1;
 
 public class Employee_wages
   { public static void main(String[] args)
-	 {   //Constant
-		 int WAGE_PER_HOUR=20;
-		 
-		 //Variables
-		 int emphours;
-		 int empwage;
-		 
-		 //Computation
-		 int empcheck= (int) (Math.floor( Math.random() * 10) % 3);
-		 switch (empcheck)
-		   {
-		    case 1 :
-		       { emphours=8; 
-		          break ; }
-		    case 2 :
-		       { emphours=8; 
-		          break ; }
-		    default :
-		       { emphours=0;
-		          break ;}
-		   }
-		 empwage= emphours*WAGE_PER_HOUR;
-		 
-		 //Printing output
-		 System.out.println("Employee status is "+empcheck);
-		 System.out.println("Employee Wage is "+empwage);
-
+	 {   //Constants
+	     int WAGE_PER_HOUR=20;
+	     int NO_OF_DAYS=20;
+	     
+	     //Variables
+	     int emphours;
+	     
+	     //Computation
+	     int empcheck = (int) (Math.floor(Math.random() * 10 ) % 3);
+	     
+	     switch (empcheck)
+	     { 
+	     case 1 :
+	     { emphours =8;
+	        break; }
+	     case 2 :
+	     { emphours=8;
+	       break ; }
+	     default :
+	     { emphours =0;
+	       break ; }
+	     }
+	     int monthly_wage = WAGE_PER_HOUR * emphours * NO_OF_DAYS ;
+	     //Printing Output
+	     System.out.println("Employee status is"+empcheck);
+	     System.out.println("Monthly Wage of Employee is "+monthly_wage);
 	   }
 }
