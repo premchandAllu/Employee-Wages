f695df04926e4ae7e2db92a4bd217842a19593f9
premchandAllu <premchandallu1999@gmail.com>
2020-09-28 08:29:45 +0530

    Added: Employee wage for 2 companies is calculated

--- Employee_wages.java
+++ Employee_wages.java
@@ -1,29 +1,21 @@
 package project1;
 
 public class Employee_wages
-  { 
-
-		 //Constants
-	      public static final  int WAGE_PER_HOUR=20;
-	      public static final int NO_OF_DAYS=20;
-	      public static final int NO_OF_HOURS=100;
+  {    
+	      //Constants
 	      public static final int IS_FULL_TIME=1;
 	      public static final int IS_PART_TIME=2;
-	      
+	   
 	      //Variables
 	      int empwage=0;
-	      int daycount=0;
-	      int hoursworked=0;
-	      int totoalwage=0;
 	      int emphours=0;
-	      
-	   public int calculateWage( )
-	    { 
-		 //Variables
-		 int daycount=0;
-         int hoursworked=0;
-         int totalwage=0;
-	     int emphours=0;
+	   
+	   public int calculateWage(int a,int b,int c )
+	    {
+		   int WAGE_PER_HOUR = a;
+		   int NO_OF_DAYS    = b;
+		   int NO_OF_HOURS   = c;
+         
 		      
 	     //Computation
 	     int empcheck = (int) (Math.floor(Math.random() * 10 ) % 3);
@@ -32,41 +24,42 @@ public class Employee_wages
 	     { 
 	     case IS_FULL_TIME :
 	     { emphours =8; 
-	       System.out.println("Employee status -Full time");
+	       System.out.println("Employee  status - Full time");
 	        break; }
 	     case IS_PART_TIME :
 	     { emphours=4;
-	       System.out.println("Employee status -Part time");
+	       System.out.println("Employee  status - Part time");
 	       break ; }
 	     default :
 	     { emphours =0;
-	       System.out.println("Employee status -Absent");
-	       System.exit(0); }
+	       System.out.println("Employee  status - Absent");
+	       break ; }
 	     }
-	    
-	     //Calculation of Daily wage
-	     empwage= emphours * WAGE_PER_HOUR;
-	     //Tabular display of Employee details
-	     
-	     while((hoursworked + emphours ) <= NO_OF_HOURS && daycount < NO_OF_DAYS)
-	        { daycount ++;
-	          totalwage =(NO_OF_HOURS- hoursworked) * WAGE_PER_HOUR;
-	          hoursworked= NO_OF_HOURS;
-	          System.out.println(" "+daycount+ " \t"+hoursworked+"  \t"+totalwage);
-	          
-	        }
-	     
-	    return totalwage;
+	     if(emphours==0)
+	     { empwage =0; }
+	     else if(NO_OF_HOURS/emphours < NO_OF_DAYS && emphours!=0)
+	     { empwage= NO_OF_HOURS* WAGE_PER_HOUR;   
+	     }
+	     else if(NO_OF_HOURS/emphours >= NO_OF_DAYS && emphours !=0)
+	     { empwage= emphours*WAGE_PER_HOUR*NO_OF_DAYS ;
+	       }
+	  
+	    return empwage;
 }
 	   public static void main(String[] args)
 	     {
 		   
-		  System.out.println("Welcome to employee wage computation ");
-		  Employee_wages obj =new Employee_wages();
-		  int Total= obj.calculateWage();
+		  Employee_wages obj1 =new Employee_wages( );
+		  int Total1 = obj1.calculateWage(20,20,100);
 		  
-		  //Display of Employee Total Wage
-		  System.out.println("Employee total wage is "+Total);
+		  //Display of Employee Total Wage for company 1
+		  System.out.println("Employee total wage for company 1 is "+Total1);
+		  
+		  Employee_wages obj2=new Employee_wages( );
+		  int Total2= obj2.calculateWage(25,20,150);
+		  
+		  //Display of Employee Total Wage for Company 2
+		  System.out.println("Employee total wage for company2 is "+Total2);
 	     }
  
   }   
