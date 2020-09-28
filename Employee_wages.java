8b32701c7b2be912c01ef8a93bf2b6a8b3621a5c
premchandAllu <premchandallu1999@gmail.com>
2020-09-28 18:25:57 +0530

    Modified: Employee wage for each company is saved

--- Employee_wages.java
+++ Employee_wages.java
@@ -9,57 +9,52 @@ public class Employee_wages
 	      //Variables
 	      int empwage=0;
 	      int emphours=0;
-	   
-	   public int calculateWage(int a,int b,int c )
-	    {
-		   int WAGE_PER_HOUR = a;
-		   int NO_OF_DAYS    = b;
-		   int NO_OF_HOURS   = c;
-         
+	    
+	public int calculateWage( String company,int WAGE_PER_HOUR,int NO_OF_DAYS,int NO_OF_HOURS )
+    {
+         //Variables
+		   int totaldays=0;
+		   int totalemphours=0;
 		      
 	     //Computation
-	     int empcheck = (int) (Math.floor(Math.random() * 10 ) % 3);
-	     
-	     switch (empcheck)
-	     { 
-	     case IS_FULL_TIME :
-	     { emphours =8; 
-	       System.out.println("Employee  status - Full time");
-	        break; }
-	     case IS_PART_TIME :
-	     { emphours=4;
-	       System.out.println("Employee  status - Part time");
-	       break ; }
-	     default :
-	     { emphours =0;
-	       System.out.println("Employee  status - Absent");
-	       break ; }
+	     	     
+	     while(totaldays<NO_OF_DAYS && totalemphours<NO_OF_HOURS)
+	     {   
+	    	 int empcheck = (int) (Math.floor(Math.random() * 10 ) % 3);
+	    
+	          switch (empcheck)
+	           { 
+	            case IS_FULL_TIME :
+	              { emphours =8; 
+	                break; }
+	            case IS_PART_TIME :
+	              { emphours=4;
+	                break ; }
+	            default :
+	              { emphours =0;
+	                break ; }
+	            }
+	          totalemphours +=emphours;
+	          
+	          //Total wage computation 
+	          empwage= totalemphours * WAGE_PER_HOUR;
 	     }
-	     if(emphours==0)
-	     { empwage =0; }
-	     else if(NO_OF_HOURS/emphours < NO_OF_DAYS && emphours!=0)
-	     { empwage= NO_OF_HOURS* WAGE_PER_HOUR;   
-	     }
-	     else if(NO_OF_HOURS/emphours >= NO_OF_DAYS && emphours !=0)
-	     { empwage= emphours*WAGE_PER_HOUR*NO_OF_DAYS ;
-	       }
-	  
 	    return empwage;
-}
+    }
 	   public static void main(String[] args)
 	     {
 		   
 		  Employee_wages obj1 =new Employee_wages( );
-		  int Total1 = obj1.calculateWage(20,20,100);
+		  int amazon = obj1.calculateWage( "Amazon",20,20,100 );
 		  
 		  //Display of Employee Total Wage for company 1
-		  System.out.println("Employee total wage for company 1 is "+Total1);
+		  System.out.println("Employee total wage for company 1 is "+amazon);
 		  
 		  Employee_wages obj2=new Employee_wages( );
-		  int Total2= obj2.calculateWage(25,20,150);
+		  int capgemini= obj2.calculateWage("Capgemini",25,20,150  );
 		  
 		  //Display of Employee Total Wage for Company 2
-		  System.out.println("Employee total wage for company2 is "+Total2);
+		  System.out.println("Employee total wage for company 2 is "+capgemini);
 	     }
  
   }   
